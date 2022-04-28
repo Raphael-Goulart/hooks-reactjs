7c2ba62e819e43c804b2410c499c86785f322cec
Raphael Goulart <raphaelnovaisgoulart@gmail.com>
2022-04-28 20:29:38 +0100

    exercicios com reactjs

---
+++ src/Components/Smartcounter/Smartcounter.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+
+function SmartCounter(){
+    //retorna um vetor
+    //1. variavel stateful
+    //2. função amarrada a essa variável que atualiza esse valor
+    const [quantity, upQuantity]= useState(1);
+
+    return (
+         <>
+            <h1>{quantity}</h1>
+            <button onClick={()=> upQuantity(quantity + 1)}>Aumentar</button>
+        </>
+    )
+}
+
+export default SmartCounter;
